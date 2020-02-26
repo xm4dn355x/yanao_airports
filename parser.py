b24eaf91795b62709d816f8e93f8ce6001a2a815
Michael Nikitenko <xm4dn355x@gmail.com>
2020-02-26 11:54:05 +0500

    added parser.py for parsing of all airports scoreboards. Created module skeleton and funcs skeletons.

---
+++ parser.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+yanao_airports.parser
+~~~~~~~~~~~~~~~
+
+This module contains parsers for airports scoreboards
+"""
+
+
+from bs4 import BeautifulSoup
+import requests
+
+
+def parse_all():
+    pass
+
+def parse_sly():
+    pass
+
+def parse_noj():
+    pass
+
+def parse_nux():
+    pass
+
+def parse_nym():
+    pass
+
+def parse_sbt():
+    pass
+
+
+if __name__ == '__main__':
+    pass
