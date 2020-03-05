c5a020fe364b6ca8f219eea100018c09221d034c
Michael Nikitenko <xm4dn355x@gmail.com>
2020-03-05 16:56:17 +0500

    added routing for this funcs to dashboard/urls.py

--- airports_boards/dashboard/urls.py
+++ airports_boards/dashboard/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sly/', views.show_sly, name="show_sly"),
+    path('noj/', views.show_noj, name="show_noj"),
+    path('nux/', views.show_nux, name="show_nux"),
+    path('nym/', views.show_nym, name="show_nym"),
+    path('sbt/', views.show_sbt, name="show_sbt"),
 ]
