c022d50f6b3e32801d27d9b7bd28b37c56fcba46
Michael Nikitenko <xm4dn355x@gmail.com>
2020-03-05 16:50:08 +0500

    Created views funcs skeletons for airports

--- airports_boards/dashboard/views.py
+++ airports_boards/dashboard/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the dashboard index.")
+
+
+def show_sly(request):
+    return HttpResponse("This is SLY Landing page")
+
+
+def show_noj(request):
+    return HttpResponse("This is NOJ landing page")
+
+
+def show_nux(request):
+    return HttpResponse("This is NUX landing page")
+
+
+def show_nym(request):
+    return HttpResponse("This is NYM landing page")
+
+
+def show_sbt(request):
+    return HttpResponse("This is SBT landing page")
