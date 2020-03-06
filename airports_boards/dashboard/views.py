633d90374023ee7722816c89468893d76e6e656f
Michael Nikitenko <xm4dn355x@gmail.com>
2020-03-06 11:39:26 +0500

    added static skeleton

--- airports_boards/dashboard/views.py
+++ airports_boards/dashboard/views.py
@@ -12,3 +12,58 @@ def index(request):
         'flights': flights
     }
     return HttpResponse(template.render(context, request))
+
+
+def show_sly(request):
+    flights = Flights.objects.all()
+    delayed = Flights.objects.all()
+    template = loader.get_template('dashboard/index.html')
+    context = {
+        'flights': flights,
+        'delayed': delayed,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def show_noj(request):
+    flights = Flights.objects.all()
+    delayed = Flights.objects.all()
+    template = loader.get_template('dashboard/index.html')
+    context = {
+        'flights': flights,
+        'delayed': delayed,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def show_nux(request):
+    flights = Flights.objects.all()
+    delayed = Flights.objects.all()
+    template = loader.get_template('dashboard/index.html')
+    context = {
+        'flights': flights,
+        'delayed': delayed,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def show_nym(request):
+    flights = Flights.objects.all()
+    delayed = Flights.objects.all()
+    template = loader.get_template('dashboard/index.html')
+    context = {
+        'flights': flights,
+        'delayed': delayed,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def show_sbt(request):
+    flights = Flights.objects.all()
+    delayed = Flights.objects.all()
+    template = loader.get_template('dashboard/index.html')
+    context = {
+        'flights': flights,
+        'delayed': delayed,
+    }
+    return HttpResponse(template.render(context, request))
