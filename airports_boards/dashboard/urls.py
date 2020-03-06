1a6ca2fa7f19fef55108e244068856675dd8e88a
Michael Nikitenko <xm4dn355x@gmail.com>
2020-03-06 11:05:50 +0500

    added new frontend to template

--- airports_boards/dashboard/urls.py
+++ airports_boards/dashboard/urls.py
@@ -5,14 +5,14 @@ from . import views
 urlpatterns = [
     #/dashboard/        index page of dashboard
     path('', views.index, name='index'),
-    #/dashboard/sly/    dashboard for SLY airport
-    path('sly/', views.show_sly, name="show_sly"),
-    # /dashboard/noj/   dashboard for NOJ airport
-    path('noj/', views.show_noj, name="show_noj"),
-    # /dashboard/nux/   dashboard for NUX airport
-    path('nux/', views.show_nux, name="show_nux"),
-    # /dashboard/nym/   dashboard for NYM airport
-    path('nym/', views.show_nym, name="show_nym"),
-    # /dashboard/sbt/   dashboard for SBT airport
-    path('sbt/', views.show_sbt, name="show_sbt"),
+    # #/dashboard/sly/    dashboard for SLY airport
+    # path('sly/', views.show_sly, name="show_sly"),
+    # # /dashboard/noj/   dashboard for NOJ airport
+    # path('noj/', views.show_noj, name="show_noj"),
+    # # /dashboard/nux/   dashboard for NUX airport
+    # path('nux/', views.show_nux, name="show_nux"),
+    # # /dashboard/nym/   dashboard for NYM airport
+    # path('nym/', views.show_nym, name="show_nym"),
+    # # /dashboard/sbt/   dashboard for SBT airport
+    # path('sbt/', views.show_sbt, name="show_sbt"),
 ]
